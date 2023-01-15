50270b793d8e6b7c684df790fd53de8ae4f97f77
dktech89 <dktech89@gmail.com>
2023-01-15 21:21:49 +0530

    Login button added

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,15 @@
 
 
 export  const Login = () =>{
+    const login =  () => {
+
+    }
     return <>
     <div>
         Login 
     </div>
     <label>User Name :</label><input /><br/>
     <label>User Name :</label> <input />
+    <button onClick={login}>Login </button>
     </>
 }
