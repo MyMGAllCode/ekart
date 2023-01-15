24de4d4d3dfb4b86e1abbabc62d6c296d57509e4
dktech89 <dktech89@gmail.com>
2023-01-15 21:16:42 +0530

    Login component added

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+
+
+
+
+export  const Login = () =>{
+    return <>
+    <div>
+        Login 
+    </div>
+    <label>User Name :</label><input /><br/>
+    <label>User Name :</label> <input />
+    </>
+}
